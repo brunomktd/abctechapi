fc740a124e096be7567d36641b0d64a588e1c593
Bruno de Oliveira <bruno.mktd@gmail.com>
2022-09-21 13:16:57 -0300

    Criação camada application

---
+++ src/main/java/br/com/fiap/abctechapi/application/impl/AssistanceApplicationImpl.java
@@ -0,0 +1,13 @@
+package br.com.fiap.abctechapi.application.impl;
+
+import br.com.fiap.abctechapi.application.AssistanceApplication;
+import br.com.fiap.abctechapi.application.dto.AssistanceDto;
+
+import java.util.List;
+
+public class AssistanceApplicationImpl implements AssistanceApplication {
+    @Override
+    public List<AssistanceDto> getAssists() {
+        return null;
+    }
+}
