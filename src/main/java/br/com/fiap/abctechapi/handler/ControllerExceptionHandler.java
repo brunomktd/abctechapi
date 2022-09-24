e94317497300674c32d6cfc7193c11959b8bf7fe
Bruno de Oliveira <bruno.mktd@gmail.com>
2022-09-23 22:32:19 -0300

    Inclusão novo handler

--- src/main/java/br/com/fiap/abctechapi/handler/ControllerExceptionHandler.java
+++ src/main/java/br/com/fiap/abctechapi/handler/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package br.com.fiap.abctechapi.handler;
 import br.com.fiap.abctechapi.handler.exception.AssistanceNotFoundException;
 import br.com.fiap.abctechapi.handler.exception.MaxAssistsException;
 import br.com.fiap.abctechapi.handler.exception.MinimumAssistsRequiredException;
+import br.com.fiap.abctechapi.handler.exception.OperatorNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -28,6 +29,11 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(AssistanceNotFoundException.class)
     public ResponseEntity<ErrorMessageResponse> assistanceNotFound(AssistanceNotFoundException exception) {
         return this.getErrorMessage("Assistance not found", exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(OperatorNotFoundException.class)
+    public ResponseEntity<ErrorMessageResponse> assistanceNotFound(OperatorNotFoundException exception) {
+        return this.getErrorMessage(exception.getMessage(), exception.getDescription(), HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity<ErrorMessageResponse> getErrorMessage(String message, String description, HttpStatus statusHttp) {
